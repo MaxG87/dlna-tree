dae8106148105ffffb6521da1853485a7c3591db
Max Görner <max+github@familie-goerner.eu>
2016-06-02 22:29:07 +0200

    Numerisches Präfix vor Einträge gestellt
    Damit sich keine Zufallsalben zwischen 'Baum' und 'Liste' schieben,
    wird vor die Einträge nun ein numerischer Präfix gestellt, der eine
    gewisse Reihenfolge sicherstellt.

--- dlna_einrichten.sh
+++ dlna_einrichten.sh
@@ -11,22 +11,23 @@ rm -rf $dlna_dir
 mkdir $dlna_dir
 
 #DLNA-Inhalte verlinken
-ln -s $musik_dir $dlna_dir/Liste
-mkdir $dlna_dir/Baum
+mkdir $dlna_dir/0_Baum
 for it in $musik_dir/*
 do
-  cur_dir=$(basename "$it")
-  ln -s "$musik_dir/$cur_dir" "$dlna_dir/Baum/$cur_dir"
+  cur_dir="$(basename "$it")"
+  ln -s "$musik_dir/$cur_dir" "$dlna_dir/0_Baum/$cur_dir"
 done
+ln -s $musik_dir $dlna_dir/1_Liste
 
 #Pseudo-BBaum bauen
-(cd $dlna_dir/Baum ; /opt/DLNA/baue_bbaum.sh)
+(cd $dlna_dir/0_Baum ; /opt/DLNA/baue_bbaum.sh)
 
 #Zufällige Ordner verlinken
 num_rand_dir=6
 shuf_arr=($(find $musik_dir -iregex '.*\(ogg\|mp3\|flac\|wma\)' -print0 | xargs -0 dirname | sort -u | shuf -n$num_rand_dir))
 for it in $(seq 0 $(($num_rand_dir - 1)))
 do
+  pre_number=$(($it+ 2))
   cur_dir="${shuf_arr[$it]}"
   album_name="$(basename "$cur_dir")"
   if echo $album_name | grep -xiE '(CD)?[ _]?[0-9]+'
@@ -35,7 +36,7 @@ do
     real_album="$(basename "$pre_dir")"
     album_name="$real_album-$album_name"
   fi
-  ln -s "$cur_dir" "$dlna_dir/$album_name"
+  ln -s "$cur_dir" "$dlna_dir/${pre_number}_$album_name"
 done
 
 sudo minidlnad -R
