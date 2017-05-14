d4cb68973f838086b9d9966f27e823f1a40ba4ed
Max Görner <max+github@familie-goerner.eu>
2017-05-14 22:56:05 +0200

    Improve recursion
    The recursion was done via Bash-Style. This is slow and quite unsuitable
    when using Python. Now a normal function call, using manipulation of the
    path string is done.

--- baue_baum.py
+++ baue_baum.py
@@ -7,17 +7,16 @@ import subprocess
 import sys
 from functools import cmp_to_key
 
-
-def main():
+def setup_node(cwd):
   locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
   len_of_shortcut = 10
   max_branching_factor = 4
-  folder_list = sorted(os.listdir('.'), key=cmp_to_key(locale.strcoll))
+  folder_list = sorted(os.listdir(cwd), key=cmp_to_key(locale.strcoll))
 
   num_elems = len(folder_list)
   if num_elems <= max_branching_factor:
     # Nothing to do
-    exit()
+    return
 
   # TODO: implement custom weight per folder
   wrap_around = True # TODO move to function signature
@@ -53,18 +52,18 @@ def main():
     branch_name = '{first_folder}_{last_folder}'.format(
                    first_folder=folder_list[cur_ind][:len_of_shortcut],
                    last_folder=folder_list[last_ind][:len_of_shortcut])
-    os.mkdir(branch_name)
+    full_branch_name = os.path.join(cwd, branch_name)
+    os.mkdir(full_branch_name)
     for folder in folder_list[cur_ind:(last_ind + 1)]:
-        shutil.move(folder, branch_name)
+        full_path = os.path.join(cwd, folder)
+        shutil.move(full_path, full_branch_name)
     cur_ind += elems_to_take
 
     # Call script recursively
-    # FIXME This is bash style recursion. It must be done better!
-    cwd = os.getcwd()
-    os.chdir(branch_name)
-    subprocess.call(os.path.join(cwd, sys.argv[0]))
-    os.chdir(cwd)
+    setup_node(full_branch_name)
 
+def main():
+  setup_node(cwd=os.getcwd())
 
 if __name__ == "__main__":
   main()
