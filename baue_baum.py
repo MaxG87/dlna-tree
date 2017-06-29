7ac778384f23a56f2f0986eecb7b31a81ecea1e1
Max Görner <max+github@familie-goerner.eu>
2017-06-29 21:13:26 +0200

    kleinere Verbesserungen
      * Kommentare
      * Code in Funktionen verschoben
      * Lesbarkeit

--- baue_baum.py
+++ baue_baum.py
@@ -3,7 +3,28 @@
 import os
 import shutil
 
+def get_access_cost(n, max_branching_factor, wrap_around):
+  if wrap_around:
+      costs = [1] + [min(n, max_branching_factor - n) + 1
+                     for n in range(1, max_branching_factor)]
+  else:
+      costs = range(1, max_branching_factor+1)
+
+    return costs
+
+
+def get_ratios(costs):
+  # Requirements:
+  #   sum(ratios) == 1
+  #   ∀i,j: ratios[i]/ratios[j] = costs[j] / costs[i]
+  ratios_unnormed = [1/c for c in costs]
+  ratios = [r/sum(ratios_unnormed) for r in ratios_unnormed]
+  assert(abs(sum(ratios) - 1) < 1e-6)
+  return ratios
+
+
 def setup_node(cwd):
+  # preparation of some constants
   len_of_shortcut = 10
   max_branching_factor = 4
   tr_dict = str.maketrans('ÄÖÜ', 'AOU')
@@ -14,19 +35,12 @@ def setup_node(cwd):
     # Nothing to do
     return
 
+  # preparation of some important variables
   # TODO: implement custom weight per folder
   wrap_around = True # TODO move to function signature
-  if wrap_around:
-      costs = [1] + [min(n, max_branching_factor - n) + 1
-                     for n in range(1, max_branching_factor)]
-  else:
-      costs = range(1, max_branching_factor+1)
-  # Requirements:
-  #   sum(ratios) == 1
-  #   ∀i,j: ratios[i]/ratios[j] = costs[j] / costs[i]
-  ratios_unnormed = [1/c for c in costs]
-  ratios = [r/sum(ratios_unnormed) for r in ratios_unnormed]
-  assert(abs(sum(ratios) - 1) < 1e-6)
+  costs = get_access_cost(n=n, max_branching_factor=max_branching_factor,
+                          wrap_around=wrap_around)
+    ratios = get_ratios(costs)
 
   # move folders to subfolders
   cur_ind = 0
@@ -38,15 +52,17 @@ def setup_node(cwd):
     # 1st Take the appropriate ratio of the elements left, but at least 1
     elems_to_take = max(1, round(cur_ratio*(num_elems-cur_ind)))
     # 2nd make sure to take all elements in last iteration
-    last_ind = (cur_ind + elems_to_take - 1 if num < len(ratios) - 1
-                else num_elems - 1) # inclusive range
+    last_ind = (
+                cur_ind + elems_to_take - 1 if num < len(ratios) - 1
+                else num_elems - 1
+               ) # inclusive range
     # 3rd make sure not to take too much elements
     last_ind = min(last_ind, num_elems - 1)
 
     if cur_ind == last_ind: # last_ind is inclusive!
         # If the subfolder would hold only one elment it is better to not
         # create it.
-        cur_ind += elems_to_take
+        cur_ind += 1
         continue
 
     branch_name = '{first_folder}_{last_folder}'.format(
