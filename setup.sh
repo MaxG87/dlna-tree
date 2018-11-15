cab632f106537fa2309a44a1c5485d41cfcbfe29
Max Görner <max@familie-goerner.eu>
2018-11-16 00:06:16 +0100

    Move setup code to function
    Even in bash long blocks of code should be separated into functions in
    order to improve scoping.

--- setup.sh
+++ setup.sh
@@ -5,13 +5,36 @@
 # can be used to automate the setup. Albeit it was tested successfully, it
 # should be used with care.
 
+function setup_server() {
+  sudo aptitude install minidlna
+
+  global_conf=/etc/minidlna.conf
+  sudo rm "$global_conf"
+  sudo ln -s "$scriptdir/minidlna.conf" "$global_conf"
+
+  # Create and populate a new group `dlnausers'. This group is needed to restrict
+  # the write access to music files as far as possible. Assuming an existing HDD
+  # with data, everything should be setup correctly. How to configure the HDD is
+  # not part of this documentation and thus currently not documented.
+  newgroup=dlnausers
+  sudo groupadd $newgroup
+  sudo adduser $USER $newgroup
+  sudo adduser minidlna $newgroup
+
+  # Add configuration lines to corresponding files
+  echo "00 4  * * * minidlna /opt/DLNA/dlna_einrichten.sh" | sudo tee -a /etc/crontab
+  echo "UUID=\"$UUID\"" "$mountdir" 'ext4 defaults,nofail 0 2' | sudo tee -a /etc/fstab
+
+  sudo mount "$mountdir"
+
+  systemctl reboot
+}
+
 if ! sudo -v
 then
   echo Root privileges are required! >&2
   exit 1
 fi
-
-sudo aptitude install minidlna
 
 scriptdir=/opt/DLNA
 mountdir=/media/Daten # Do not change lighthearted. Unfortunately, this path is
@@ -32,26 +55,5 @@ UUID=e69f73d8-44f3-4cf9-b965-c6a35fde05e5
 # 4. Adapt the entry to /etc/fstab by adding the appropriate UUID.
 
 sudo mkdir -p "$scriptdir" "$mountdir"
-
-# TODO Move files to $scriptdir.
-
-global_conf=/etc/minidlna.conf
-sudo rm "$global_conf"
-sudo ln -s "$scriptdir/minidlna.conf" "$global_conf"
-
-# Create and populate a new group `dlnausers'. This group is needed to restrict
-# the write access to music files as far as possible. Assuming an existing HDD
-# with data, everything should be setup correctly. How to configure the HDD is
-# not part of this documentation and thus currently not documented.
-newgroup=dlnausers
-sudo groupadd $newgroup
-sudo adduser $USER $newgroup
-sudo adduser minidlna $newgroup
-
-# Add configuration lines to corresponding files
-echo "00 4  * * * minidlna /opt/DLNA/dlna_einrichten.sh" | sudo tee -a /etc/crontab
-echo "UUID=\"$UUID\"" "$mountdir" 'ext4 defaults,nofail 0 2' | sudo tee -a /etc/fstab
-
-sudo mount "$mountdir"
-
-systemctl reboot
+# TODO Move DLNA files to $scriptdir.
+setup_server
